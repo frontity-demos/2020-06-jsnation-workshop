d6626d0429e7c20f6b6e169cbb71bff5e3c4f0e5
Michael Burridge <mburridge@zyriab.co.uk>
2020-06-09 18:21:54 +0100

    adds PostInfo section to post.js

--- packages/jsnation-theme/src/theme-files/post.js
+++ packages/jsnation-theme/src/theme-files/post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 
 const Post = ({ state }) => {
     const data = state.source.get(state.router.link)
@@ -9,11 +9,24 @@ const Post = ({ state }) => {
     return (
         <div>
             <h2>{post.title.rendered}</h2>
-            <p><strong>Posted: </strong>{post.date}</p>
-            <p><strong>Author: </strong>{author.name}</p>
+            <PostInfo>
+                <p><strong>Posted: </strong>{post.date}</p>
+                <p><strong>Author: </strong>{author.name}</p>
+            </PostInfo>
             <div dangerouslySetInnerHTML={{ __html: post.content.rendered}} />
         </div>
     )
 }
 
 export default connect(Post)
+
+const PostInfo = styled.div`
+    background-image: linear-gradient(to right, #f4f4f4, #fff);
+    margin-bottom: 1em;
+    padding: 0.5em;
+    border-left: 4px solid lightseagreen;
+
+    & > p {
+        margin: 0;
+    }
+`
