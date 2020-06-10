c40734ea0199de5f14513ce649d76c62a618a1ae
Michael Burridge <mburridge@zyriab.co.uk>
2020-06-10 12:03:43 +0100

    adds previous/next navigation to list.js

--- packages/jsnation-theme/src/theme-files/list.js
+++ packages/jsnation-theme/src/theme-files/list.js
@@ -2,7 +2,7 @@ import React from "react"
 import { connect, styled } from "frontity"
 import Link from "./link"
 
-const List = ({ state }) => {
+const List = ({ state, actions }) => {
     const data = state.source.get(state.router.link)
 
     return (
@@ -15,6 +15,16 @@ const List = ({ state }) => {
                     </Link>
                 )
             })}
+            <PrevNextNav>
+                { data.previous ? <button onClick={e => {
+                    e.preventDefault()
+                    actions.router.set(data.previous)
+                }}>&#171; Prev</button> : ' ' }
+                { data.next ? <button onClick={e => {
+                    e.preventDefault()
+                    actions.router.set(data.next)
+                }}>Next &#187;</button> : ' ' }
+            </PrevNextNav>
         </Items>
     )
 }
@@ -25,5 +35,22 @@ const Items = styled.div`
     & > div {
         margin: 12px 0;
         font-size: 1.2em;
+    }
+`
+const PrevNextNav = styled.div`
+
+    padding-top: 1.5em;
+
+    & > button {
+        background: #eee;
+        text-decoration: none;
+        padding: 0.5em 1em;
+        color: #888;
+        border: 1px solid #aaa;
+        font-size: 0.8em;
+        margin-right: 2em;
+    }
+    & > button:hover {
+        cursor: pointer;
     }
 `
